cdc13eceb2c418433181f343ea15f310d7fd0244
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-09-26 15:45:00 -0300

    text changes

--- src/page/fourthBlock.tsx
+++ src/page/fourthBlock.tsx
@@ -12,11 +12,11 @@ export function FourthBlock() {
                     <li className="text-lg">Suporte e Desenvolvimento BemMelhor SI</li>
                     <p className="text-sm mb-8">Trabalhando com Back-End da telefonia AsteriskIPBX puro, Node ,Javascript,Mongo e Postgree</p>
                     <li className="text-lg">Formando do curso de programação Full Stack da Driven Education</li>
-                    <p className="text-sm mb-8">Além de reforçar meis conhecimentos aprendi várias coisas novas e me preparei mais ainda para o mercado de trabalho</p>
+                    <p className="text-sm mb-8">Além de reforçar meus conhecimentos aprendi várias coisas novas e me preparei mais ainda para o mercado de trabalho</p>
                     <li className="text-lg">Bolsista do Centro Regional de Inovação e Transferência de Tecnologia</li>
                     <p className="text-sm mb-8">Aqui aconteceu minha inserção no mercado de inovação e empreendedorismo, assim como interagi com servidores , proteção de dados LGPD, desenvolvimento de software para melhora do workflow e banco de dados</p>
                     <li className="text-lg">Bacharelado em Ciências Exatas pela UFJF</li>
-                    <p className="text-sm mb-8">Trabalhando com Back-End da telefonia AsteriskIPBX puro, Node ,Javascript,Mongo e Postgree</p>
+                    <p className="text-sm mb-8">Formando da UFJF na area de Ciências Exatas</p>
                 </div>
             </div>
 
